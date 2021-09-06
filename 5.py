ce992b0802e20f35606d6951186a634824831879
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-09-06 17:00:10 -0300

    Create 5.py
    questao prova

---
+++ 5.py
@@ -0,0 +1,38 @@
+a = input("Digite um nome de um filme ")
+b = input("Digite um nome de um filme ")
+
+filmes = [a,b]
+print(filmes)
+
+c = input("Digite um nome de um jogo ")
+d = input("Digite um nome de um filme ")
+
+jogos = [c,d]
+print(jogos)
+
+e = input("Digite um nome de um livro ")
+f = input("Digite um nome de um livro ")
+
+livros = [e,f]
+print(filmes)
+
+g = input("Digite um nome de um esporte ")
+h = input("Digite um nome de um esporte ")
+
+esporte = [g,h]
+print(esporte)
+
+listacp = filmes.copy()
+print( "lista copiada",listacp)
+
+listacp = jogos.copy()
+print( "lista copiada",listacp)
+
+listacp = livros.copy()
+print( "lista copiada",listacp)
+
+listacp = esporte.copy()
+print( "lista copiada",listacp)
+
+
+
