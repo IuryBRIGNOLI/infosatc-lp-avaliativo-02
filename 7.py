d0764c80090b8f430e5fceeca7f725175423d0cc
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-09-06 17:05:19 -0300

    Create 7.py
    questao prova

---
+++ 7.py
@@ -0,0 +1,11 @@
+n1 = input("Digite um valor inteiro ")
+n2 = input("Digite um valor inteiro ")
+n3 = input("Digite um valor inteiro ")
+n4 = input("Digite um valor inteiro ")
+n5 = input("Digite um valor inteiro ")
+n6 = input("Digite um valor inteiro ")
+
+nmr = [n1,n2,n3,n4,n5,n6]
+print(nmr)
+
+print("Números organizados",sorted(nmr))
