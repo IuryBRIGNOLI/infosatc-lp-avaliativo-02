47160ece2fe9f66a7d79a116054935adce85d960
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-09-06 16:22:55 -0300

    Create 2.py
    questo lista 2

---
+++ 2.py
@@ -0,0 +1,9 @@
+n1 = input("Digite um carro ")
+n2 = input("Digite um carro ")
+n3 = input("Digite um carro ")
+
+lista = [n1,n2,n3]
+print(lista)
+
+lista2 = lista.copy()
+print( "lista copiada",lista2)
