f1027b160f89c6405452e4253fa7a1504f14ed05
IuryBRIGNOLI <iury.195018@alunosatc.edu.br>
2021-09-06 15:51:21 -0300

    Create 1.py
    Primeira questão

---
+++ 1.py
@@ -0,0 +1,11 @@
+n1 = float(input("Digite um valor"))
+n2 = float(input("Digite um valor"))
+n3 = float(input("Digite um valor"))
+n4 = float(input("Digite um valor"))
+n5 = float(input("Digite um valor"))
+
+lista =  [n1,n2,n3,n4,n5]
+print("Os números  digitados são ",lista)
+
+soma = sum(lista)
+print(soma)
